6ea2602b03d4733b169dfb53547d45d3ca678df5
Bradley Bonitatibus <bradbonitatibus@gmail.com>
2019-11-10 10:28:16 -0500

    Runs correctly after fixing lib import

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+mod lib;
+use lib::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let conf: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", conf.query);
+    println!("In file: {}", conf.filename);
+
+    if let Err(e) = lib::run(conf) {
+        println!("Application error: {}", e);
+    }
+}
+
+
